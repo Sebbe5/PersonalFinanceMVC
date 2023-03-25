8c5da21fa2286ed3e35fa2febf3c04b2e3fcc2c9
Sebastian Fredriksson <sebastian.fredriksson1996@gmail.com>
2023-03-25 19:23:47 +0100

    Added promts and max length

--- Views/Login/RegisterVM.cs
+++ Views/Login/RegisterVM.cs
@@ -4,11 +4,12 @@ namespace PersonalFinanceMVC.Views.Login
 {
     public class RegisterVM
     {
-        [Display(Name = "Username")]
+        [Display(Name = "Username", Prompt = "Max 12 characters")]
         [Required]
+        [MaxLength(12)]
         public string Username { get; set; }
 
-        [Display(Name = "Password")]
+        [Display(Name = "Password", Prompt = "1 capital, 1 special character")]
         [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
