01c93443c25f864e22a59479982afa4fdaee92e9
Sebastian Fredriksson <sebastian.fredriksson1996@gmail.com>
2023-04-15 16:36:25 +0200

    Added userid for dataservice

--- Models/DataService.cs
+++ Models/DataService.cs
@@ -1,13 +1,23 @@
-﻿using PersonalFinanceMVC.Models.Entities;
+﻿using Microsoft.AspNetCore.Identity;
+using PersonalFinanceMVC.Models.Entities;
 
 namespace PersonalFinanceMVC.Models
 {
     public class DataService
     {
+        UserManager<ApplicationUser> userManager;
+        readonly string userId;
+
         private readonly ApplicationContext context;
-        public DataService(ApplicationContext context)
+        public DataService(
+            ApplicationContext context,
+            UserManager<ApplicationUser> userManager,
+            IHttpContextAccessor accessor
+            )
         {
             this.context = context;
+            this.userManager = userManager;
+            userId = userManager.GetUserId(accessor.HttpContext.User);
         }
     }
 }
