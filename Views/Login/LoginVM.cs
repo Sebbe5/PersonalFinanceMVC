0f00226f45ca100dedbd049cb34ea304811bcb46
Sebastian Fredriksson <sebastian.fredriksson1996@gmail.com>
2023-04-16 16:33:40 +0200

    css

--- Views/Login/LoginVM.cs
+++ Views/Login/LoginVM.cs
@@ -5,9 +5,11 @@ namespace PersonalFinanceMVC.Views.Login
     public class LoginVM
     {
         [Required(ErrorMessage = "Please enter a username.")]
+        [Display(Prompt = "Username")]
         public string Username { get; set; }
 
         [Required(ErrorMessage = "Please enter a password.")]
+        [Display(Prompt = "Password")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
     }
