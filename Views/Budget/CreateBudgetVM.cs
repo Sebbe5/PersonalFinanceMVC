02964fdd7e80bca78801139d9f3402398b580828
Sebastian Fredriksson <sebastian.fredriksson1996@gmail.com>
2023-04-18 10:13:40 +0200

    Added propper attributes

--- Views/Budget/CreateBudgetVM.cs
+++ Views/Budget/CreateBudgetVM.cs
@@ -4,6 +4,7 @@ namespace PersonalFinanceMVC.Views.Budget
 {
     public class CreateBudgetVM
     {
+        [Display(Name = "Budget Name:")]
         [Required(ErrorMessage = "Please enter a budget name.")]
         public string Name { get; set; }
         public List<ExpenseItemVM> Expenses { get; set; } = new List<ExpenseItemVM>();
