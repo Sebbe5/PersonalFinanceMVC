9155c0eb440c880dbd8154d222babe154342533d
Sebastian Fredriksson <sebastian.fredriksson1996@gmail.com>
2023-04-18 10:04:48 +0200

    Fixed proper attributes

--- Views/Compound/CalculateVM.cs
+++ Views/Compound/CalculateVM.cs
@@ -1,18 +1,35 @@
-﻿namespace PersonalFinanceMVC.Views.Compound
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonalFinanceMVC.Views.Compound
 {
     public class CalculateVM
     {
+        [Display(Name = "Principal Amount:")]
         public decimal Principal { get; set; }
+
+        [Display(Name = "Monthly Contribution:")]
         public decimal MonthlyContribution { get; set; }
+
+        [Display(Name = " Annual interest rate (%):")]
         public decimal Rate { get; set; }
+
+        [Display(Name = "Amount of years:")]
         public int Years { get; set; }
+
         public List<CompoundInterestResult> Results { get; set; } = new List<CompoundInterestResult>();
 
         public class CompoundInterestResult
         {
+            [Display(Name = "Total contribution:")]
             public decimal Contribution { get; set; }
+
+            [Display(Name = "Year")]
             public int Year { get; set; }
+
+            [Display(Name = "Interest")]
             public decimal Interest { get; set; }
+
+            [Display(Name = "Amount")]
             public decimal Amount { get; set; }
         }
     }
