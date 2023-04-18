24cd2fc3bcd1e30e37dd4f4652df7cea0c04d97e
Sebastian Fredriksson <sebastian.fredriksson1996@gmail.com>
2023-04-18 10:53:44 +0200

    cleanup

--- Views/Compound/CalculateVM.cs
+++ Views/Compound/CalculateVM.cs
@@ -4,32 +4,28 @@ namespace PersonalFinanceMVC.Views.Compound
 {
     public class CalculateVM
     {
-        [Display(Name = "Principal Amount:")]
+        [Display(Name = "Principal Amount")]
         public decimal Principal { get; set; }
 
-        [Display(Name = "Monthly Contribution:")]
+        [Display(Name = "Monthly Contribution")]
         public decimal MonthlyContribution { get; set; }
 
-        [Display(Name = " Annual interest rate (%):")]
+        [Display(Name = "Annual interest rate (%)")]
         public decimal Rate { get; set; }
 
-        [Display(Name = "Amount of years:")]
+        [Display(Name = "Number of years")]
         public int Years { get; set; }
 
         public List<CompoundInterestResult> Results { get; set; } = new List<CompoundInterestResult>();
 
         public class CompoundInterestResult
         {
-            [Display(Name = "Total contribution:")]
             public decimal Contribution { get; set; }
 
-            [Display(Name = "Year")]
             public int Year { get; set; }
 
-            [Display(Name = "Interest")]
             public decimal Interest { get; set; }
 
-            [Display(Name = "Amount")]
             public decimal Amount { get; set; }
         }
     }
--- Views/Login/RegisterVM.cs
+++ Views/Login/RegisterVM.cs
@@ -5,23 +5,23 @@ namespace PersonalFinanceMVC.Views.Login
     public class RegisterVM
     {
         [Required(ErrorMessage = "The {0} field is required.")]
-        [Display(Prompt = "Username")]
+        [Display(Name = "Username", Prompt = "Username")]
         [StringLength(12, ErrorMessage = "The {0} must not exceed {1} characters.")]
         public string Username { get; set; }
 
         [Required(ErrorMessage = "The {0} field is required.")]
-        [Display(Prompt = "E-mail")]
+        [Display(Name = "E-Mail", Prompt = "E-mail")]
         [EmailAddress(ErrorMessage = "The {0} field is not a valid email address.")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "The {0} field is required.")]
-        [Display(Prompt = "Password")]
+        [Display(Name= "Password", Prompt = "Password")]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Prompt = "Confirm Password")]
+        [Display(Name = "Confirm Password", Prompt = "Confirm Password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
