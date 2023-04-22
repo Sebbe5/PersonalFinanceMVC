3a27a0cfb1a7512d96f96c3d7507b562a352c731
Sebastian Fredriksson <sebastian.fredriksson1996@gmail.com>
2023-04-22 17:16:20 +0200

    removed todo category fully from database

---
+++ Migrations/20230422151533_Remove categorytodo on todo.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonalFinanceMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class Removecategorytodoontodo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TodoCategoryId",
+                table: "Todos");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TodoCategoryId",
+                table: "Todos",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
