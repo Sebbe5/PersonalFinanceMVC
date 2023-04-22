22ee9b91b4f10b7fcfc51354daa5d8c29ed90c9e
Sebastian Fredriksson <sebastian.fredriksson1996@gmail.com>
2023-04-22 17:22:15 +0200

    add category

--- Models/Entities/Todo.cs
+++ Models/Entities/Todo.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime Deadline { get; set; }
-        public int? TodoCategoryId { get; set; }
+        public string Category { get; set; }
         public string ApplicationUserId { get; set; }
         ApplicationUser ApplicationUser { get; set; }
     }
