5bd74df6af4528e2181732868349c8979bab65b3
Sebastian Fredriksson <sebastian.fredriksson1996@gmail.com>
2023-04-22 16:35:07 +0200

    -

---
+++ Migrations/20230422135934_Add categories to user.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonalFinanceMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addcategoriestouser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ApplicationUserTodoCategory",
+                columns: table => new
+                {
+                    PreferredTodoCategoriesId = table.Column<int>(type: "int", nullable: false),
+                    UsersId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApplicationUserTodoCategory", x => new { x.PreferredTodoCategoriesId, x.UsersId });
+                    table.ForeignKey(
+                        name: "FK_ApplicationUserTodoCategory_AspNetUsers_UsersId",
+                        column: x => x.UsersId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ApplicationUserTodoCategory_TodoCategory_PreferredTodoCategoriesId",
+                        column: x => x.PreferredTodoCategoriesId,
+                        principalTable: "TodoCategory",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApplicationUserTodoCategory_UsersId",
+                table: "ApplicationUserTodoCategory",
+                column: "UsersId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ApplicationUserTodoCategory");
+        }
+    }
+}
---
+++ Migrations/20230422141302_Fix many to many.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonalFinanceMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fixmanytomany : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ApplicationUserTodoCategory_AspNetUsers_UsersId",
+                table: "ApplicationUserTodoCategory");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ApplicationUserTodoCategory_TodoCategory_PreferredTodoCategoriesId",
+                table: "ApplicationUserTodoCategory");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ApplicationUserTodoCategory",
+                table: "ApplicationUserTodoCategory");
+
+            migrationBuilder.RenameTable(
+                name: "ApplicationUserTodoCategory",
+                newName: "UserTodoCategory");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ApplicationUserTodoCategory_UsersId",
+                table: "UserTodoCategory",
+                newName: "IX_UserTodoCategory_UsersId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserTodoCategory",
+                table: "UserTodoCategory",
+                columns: new[] { "PreferredTodoCategoriesId", "UsersId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserTodoCategory_AspNetUsers_UsersId",
+                table: "UserTodoCategory",
+                column: "UsersId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserTodoCategory_TodoCategory_PreferredTodoCategoriesId",
+                table: "UserTodoCategory",
+                column: "PreferredTodoCategoriesId",
+                principalTable: "TodoCategory",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserTodoCategory_AspNetUsers_UsersId",
+                table: "UserTodoCategory");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserTodoCategory_TodoCategory_PreferredTodoCategoriesId",
+                table: "UserTodoCategory");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserTodoCategory",
+                table: "UserTodoCategory");
+
+            migrationBuilder.RenameTable(
+                name: "UserTodoCategory",
+                newName: "ApplicationUserTodoCategory");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserTodoCategory_UsersId",
+                table: "ApplicationUserTodoCategory",
+                newName: "IX_ApplicationUserTodoCategory_UsersId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ApplicationUserTodoCategory",
+                table: "ApplicationUserTodoCategory",
+                columns: new[] { "PreferredTodoCategoriesId", "UsersId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ApplicationUserTodoCategory_AspNetUsers_UsersId",
+                table: "ApplicationUserTodoCategory",
+                column: "UsersId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ApplicationUserTodoCategory_TodoCategory_PreferredTodoCategoriesId",
+                table: "ApplicationUserTodoCategory",
+                column: "PreferredTodoCategoriesId",
+                principalTable: "TodoCategory",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
