e45fb1e8ebbf15e035e8ce6e88b5504ba924af10
Sebastian Fredriksson <sebastian.fredriksson1996@gmail.com>
2023-04-25 11:28:00 +0200

    CleanUp with partial views

---
+++ Views/Shared/_FunctionCardVM.cs
@@ -0,0 +1,12 @@
+﻿namespace PersonalFinanceMVC.Views.Shared
+{
+    public class _FunctionCardVM
+    {
+        public string Image { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public string FunctionName { get; set; }
+    }
+}
