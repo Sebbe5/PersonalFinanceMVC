7eaeae2921dd9f475607bcb6790b5928482bd9cc
Sebastian Fredriksson <sebastian.fredriksson1996@gmail.com>
2023-04-27 14:13:28 +0200

    base for controller

--- Controllers/InvestmentController.cs
+++ Controllers/InvestmentController.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PersonalFinanceMVC.Models;
 
 namespace PersonalFinanceMVC.Controllers
 {
     public class InvestmentController : Controller
     {
+        InvestmentService investmentService;
+        public InvestmentController(InvestmentService dataService)
+        {
+            this.investmentService = dataService;
+        }
         public IActionResult Index()
         {
             return View();
