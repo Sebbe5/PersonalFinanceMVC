4455aa9e1e18c84aa2386bbe6c433c615070f325
Sebastian Fredriksson <sebastian.fredriksson1996@gmail.com>
2023-04-27 14:02:37 +0200

    investment service

--- Models/InvestmentService.cs
+++ Models/InvestmentService.cs
@@ -1,6 +1,22 @@
-﻿namespace PersonalFinanceMVC.Models
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace PersonalFinanceMVC.Models
 {
     public class InvestmentService
     {
+        UserManager<ApplicationUser> userManager;
+        readonly string userId;
+
+        private readonly ApplicationContext context;
+        public InvestmentService(
+            ApplicationContext context,
+            UserManager<ApplicationUser> userManager,
+            IHttpContextAccessor accessor
+            )
+        {
+            this.context = context;
+            this.userManager = userManager;
+            userId = userManager.GetUserId(accessor.HttpContext.User);
+        }
     }
 }
