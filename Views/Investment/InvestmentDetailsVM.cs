73cf79bc6c411670fd47e188c13f26ccaeb9c6d0
Sebastian Fredriksson <sebastian.fredriksson1996@gmail.com>
2023-05-02 17:44:19 +0200

    -

--- Views/Investment/InvestmentDetailsVM.cs
+++ Views/Investment/InvestmentDetailsVM.cs
@@ -1,6 +1,14 @@
-﻿namespace PersonalFinanceMVC.Views.Investment
+﻿using Microsoft.CodeAnalysis.Scripting.Hosting;
+
+namespace PersonalFinanceMVC.Views.Investment
 {
     public class InvestmentDetailsVM
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<double> Contributions { get; set; } = new List<double>();
+        public List<double> Profit { get; set; } = new List<double>();
+
+        // TODO: Continue with this vm
     }
 }
