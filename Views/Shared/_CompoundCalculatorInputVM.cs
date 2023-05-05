dffb5d38021231ab6b2f9c94564240d412acb0e4
Sebastian Fredriksson <sebastian.fredriksson1996@gmail.com>
2023-05-05 15:23:41 +0200

    Added value to partial view

--- Views/Shared/_CompoundCalculatorInputVM.cs
+++ Views/Shared/_CompoundCalculatorInputVM.cs
@@ -7,5 +7,6 @@ namespace PersonalFinanceMVC.Views.Shared
         public string Name { get; set; }
         public string Description { get; set; }
         public string Title { get; set; }
+        public decimal Value { get; set; }
     }
 }
