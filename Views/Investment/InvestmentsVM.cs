f2944fd16b3826db63978cef23bd67c2fd6f0bee
Sebastian Fredriksson <sebastian.fredriksson1996@gmail.com>
2023-05-06 11:51:07 +0200

    added todo

--- Views/Investment/InvestmentsVM.cs
+++ Views/Investment/InvestmentsVM.cs
@@ -2,6 +2,8 @@
 {
     public class InvestmentsVM
     {
+
+        // TODO: Add expected years invested (and add everything that should follow from it)
         public List<InvestmentItemVM> Investments { get; set; } = new List<InvestmentItemVM>();
         public class InvestmentItemVM
         {
