3803b38c744bb87c07c9a2b1bc6807989fe28271
Sebastian Fredriksson <sebastian.fredriksson1996@gmail.com>
2023-05-08 18:26:32 +0200

    removed unused prop

--- Views/Investment/InvestmentsVM.cs
+++ Views/Investment/InvestmentsVM.cs
@@ -10,7 +10,6 @@
             public int Id { get; set; }
             public string Name { get; set; }
             public double Value { get; set; }
-            public int ExpectedYearsInvested { get; set; }
         }
     }
 }
