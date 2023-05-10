d42e70c61f8d38b844b75929c8a9a6e1b5a05dae
Sebastian Fredriksson <sebastian.fredriksson1996@gmail.com>
2023-05-10 11:32:18 +0200

    removed todo

--- Views/Investment/InvestmentsVM.cs
+++ Views/Investment/InvestmentsVM.cs
@@ -2,8 +2,6 @@
 {
     public class InvestmentsVM
     {
-
-        // TODO: Add expected years invested (and add everything that should follow from it)
         public List<InvestmentItemVM> Investments { get; set; } = new List<InvestmentItemVM>();
         public class InvestmentItemVM
         {
