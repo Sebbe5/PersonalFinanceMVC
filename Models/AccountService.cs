09199b0069177da899e9b78029b68cf057b4cbc8
Sebastian Fredriksson <sebastian.fredriksson1996@gmail.com>
2023-06-09 08:24:04 +0200

    AddedComments

--- Models/AccountService.cs
+++ Models/AccountService.cs
@@ -31,44 +31,51 @@ namespace PersonalFinanceMVC.Models
 
         public async Task<string> TryLoginAsync(LoginVM viewModel)
         {
+            // Check if the password is null
             if (viewModel.Password == null)
             {
-                return null;
+                return null; // Return null if the password is null
             }
+
+            // Try to sign in using the provided username and password
             SignInResult result = await signInManager.PasswordSignInAsync(
                 viewModel.Username,
                 viewModel.Password,
                 isPersistent: false,
                 lockoutOnFailure: false);
-            if (result.Succeeded)
-                return null;
-            else
-                return "Either the username or password is incorrect";
-        }
 
+            // Check if the sign-in attempt succeeded
+            if (result.Succeeded)
+                return null; // Return null if the sign-in was successful
+            else
+                return "Either the username or password is incorrect"; // Return an error message if the sign-in failed
+        }
         public async Task<string> TryRegisterAsync(RegisterVM viewModel)
         {
-            // TODO: Start commenting here
+            // Create a new instance of the ApplicationUser with the provided username and email
             var user = new ApplicationUser
             {
                 UserName = viewModel.Username,
                 Email = viewModel.Email,
             };
 
+            // Check if the password is null
             if (viewModel.Password == null)
             {
-                return null;
+                return null; // Return null if the password is null
             }
-            IdentityResult result = await
-                userManager.CreateAsync(user, viewModel.Password);
 
+            // Attempt to create a new user with the provided user object and password
+            IdentityResult result = await userManager.CreateAsync(user, viewModel.Password);
+
+            // Check if the user creation was successful
             return result.Errors.FirstOrDefault()?.Description;
+            // Return the description of the first error, if any, or null if the creation was successful
         }
-
         internal void SignOut()
         {
+            // Initiate the sign out process asynchronously
             var result = signInManager.SignOutAsync();
         }
-
     }
 }
