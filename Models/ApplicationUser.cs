f7a97c4eab8b96568fc8e6c47d02af26a85e1d12
Sebastian Fredriksson <sebastian.fredriksson1996@gmail.com>
2023-06-10 07:32:17 +0200

    AddedTodo

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -5,6 +5,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PersonalFinanceMVC.Models
 {
+    // TODO: COntinue commenting here
     public class ApplicationUser : IdentityUser
     {
         public List<Budget> Budgets { get; set; }
