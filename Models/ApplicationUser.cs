970ec0bd135653bd50637888206d0eb6141633f6
Sebastian Fredriksson <sebastian.fredriksson1996@gmail.com>
2023-06-11 18:56:45 +0200

    AddedComments

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -5,14 +5,22 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PersonalFinanceMVC.Models
 {
-    // TODO: COntinue commenting here
     public class ApplicationUser : IdentityUser
     {
+        // The Budgets property represents a collection of budgets associated with the user.
         public List<Budget> Budgets { get; set; }
+
+        // The Todos property represents a collection of to-do items associated with the user.
         public List<Todo> Todos { get; set; }
+
+        // The TodoSortingOrder property represents the sorting order for the to-do items.
+        // It is of type TodoSortOrder, which is an enumeration defined below.
+        // The default sorting order is set to AscendingName.
         public TodoSortOrder TodoSortingOrder { get; set; } = new TodoSortOrder();
     }
 
+    // The TodoSortOrder enumeration represents the possible sorting orders for to-do items.
+    // It defines four options: AscendingName, DescendingName, AscendingDate, and DescendingDate.
     public enum TodoSortOrder
     {
         AscendingName,
